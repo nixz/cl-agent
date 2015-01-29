29ff2ee594fd3e760c2bc289117bc525b555c658
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2015-01-28 21:46:20 -0700

    removing extra packages and replacing with nicknames

--- package.lisp
+++ package.lisp
@@ -1,17 +1,5 @@
 (in-package #:cl-user)
 
 (defpackage #:cl-agent
-  (:use #:cl
-        #:org))
-
-(defpackage #:org
-  (:use #:cl
-        #:org.fipa))
-
-(defpackage #:org.fipa
-  (:use #:cl
-        #:org.fipa.std))
-
-
-
-
+  (:nicknames #:org.fipa #:fipa)
+  (:use #:cl))
